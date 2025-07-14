91fa5f26b20ab7ccb41f2f6254ce581eea327402
Song-Young-Ho <sng4929@naver.com>
2025-07-14 14:04:35 +0900

    extract interface - outputHandler

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -1,14 +1,19 @@
 package cleancode.minesweeper.tobe;
 
 import cleancode.minesweeper.tobe.gamelevel.Beginner;
+import cleancode.minesweeper.tobe.io.ConsoleInputHandler;
+import cleancode.minesweeper.tobe.io.ConsoleOutputHandler;
+import cleancode.minesweeper.tobe.io.InputHandler;
+import cleancode.minesweeper.tobe.io.OutputHandler;
 
 public class GameApplication {
     public static void main(String[] args) {
         Beginner beginner = new Beginner();
+        InputHandler inputHandler = new ConsoleInputHandler();
+        OutputHandler outputHandler = new ConsoleOutputHandler();
 
-        MineSweeper mineSweeper = new MineSweeper(beginner);
+        MineSweeper mineSweeper = new MineSweeper(beginner, inputHandler, outputHandler);
         mineSweeper.initialize();
         mineSweeper.run();
     }
-
 }
---
+++ src/main/java/cleancode/minesweeper/tobe/io/OutputHandler.java
@@ -0,0 +1,22 @@
+package cleancode.minesweeper.tobe.io;
+
+import cleancode.minesweeper.tobe.GameBoard;
+import cleancode.minesweeper.tobe.GameException;
+
+public interface OutputHandler {
+    void showGameStartComments();
+
+    void showBoard(GameBoard board);
+
+    void showGameWinningComment();
+
+    void printGameLosingComment();
+
+    void showCommentForSelectingCell();
+
+    void showCommentForUserAction();
+
+    void showExceptionMessage(GameException e);
+
+    void showSimpleMessage(String message);
+}
